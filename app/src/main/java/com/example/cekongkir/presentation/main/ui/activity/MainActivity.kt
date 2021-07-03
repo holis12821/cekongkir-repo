05a91ba4047810e4bb6c9368efe23d2b85bb7861
Nurholis <holis.nur072219@gmail.com>
2021-07-04 01:19:59 +0700

    Create a folder to implement Clean Architecture and move the file to the appropriate folder and create an author in each file

--- app/src/main/java/com/example/cekongkir/presentation/main/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/cekongkir/presentation/main/ui/activity/MainActivity.kt
@@ -1,8 +1,14 @@
-package com.example.cekongkir
+/**
+ * Cek Ongkir Android Apps
+ * Copyright (c) 2021. All rights reserved.
+ * Created by Nurholis on 4/7/21 01:07 AM
+ * Last modified 4/7/21 01:08 PM by Nurholis*/
+package com.example.cekongkir.presentation.main.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cekongkir.ui.main.MainFragment
+import com.example.cekongkir.R
+import com.example.cekongkir.presentation.main.ui.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/cekongkir/presentation/main/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/example/cekongkir/presentation/main/ui/fragment/MainFragment.kt
@@ -1,4 +1,9 @@
-package com.example.cekongkir.ui.main
+/**
+ * Cek Ongkir Android Apps
+ * Copyright (c) 2021. All rights reserved.
+ * Created by Nurholis on 4/7/21 01:07 AM
+ * Last modified 4/7/21 01:08 PM by Nurholis*/
+package com.example.cekongkir.presentation.main.ui.fragment
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
