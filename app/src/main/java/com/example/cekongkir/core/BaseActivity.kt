a971555f2e906c34ca9450d4780dc23836851140
Nurholis <holis.nur072219@gmail.com>
2021-07-10 03:31:33 +0700

    Add coding in BaseActivity.kt

--- app/src/main/java/com/example/cekongkir/core/BaseActivity.kt
+++ app/src/main/java/com/example/cekongkir/core/BaseActivity.kt
@@ -8,5 +8,19 @@
  * rewriting of source code.*/
 package com.example.cekongkir.core
 
-class BaseActivity {
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+
+/**
+ * This BaseActivity class is used for activity that using view
+ * and data binding by extending this class will have common function for
+ * binding the view.
+ * For extend this class we need two parameter
+ * @param B for view data binding and
+ * @ViewDataBinding is both parameter required and can't
+ * set to null. This parameter is generic constraint type parameter which
+ * is used to restrict what generic types to attach.
+ * This Generic type is the same of Bounded Parameter Type in Java*/
+abstract class BaseActivity<B : ViewDataBinding>: AppCompatActivity() {
+
 }
