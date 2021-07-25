21f2ab6bacf1ae68cd072e0fc310b979787c17d9
Nurholis <holis.nur072219@gmail.com>
2021-07-25 14:44:35 +0700

    Fixing KoinModuleTest for testing core module and refactoring library koin dependency injection in build gradle

--- app/src/main/java/com/example/cekongkir/di/ViewModelModule.kt
+++ app/src/main/java/com/example/cekongkir/di/ViewModelModule.kt
@@ -1,9 +1,17 @@
+/**
+ * Cek Ongkir Android Apps
+ * Copyright (c) 2021. All rights reserved.
+ * @author Created by Nurholis on 25/7/21 14:41 PM
+ * Last modified 25/7/21 14:41 PM by Nurholis
+ * Licensed under the Apache Licence, Version 2.0 (the "License");
+ * May not copy files in their entirety for either distribution or
+ * rewriting of source code.*/
 package com.example.cekongkir.di
 
 import com.example.cekongkir.presentation.ui.fragment.fragmentcity.FragmentCityViewModel
-import com.example.cekongkir.presentation.ui.fragment.fragmentcity.FragmentCityViewState
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    single { FragmentCityViewModel(get()) }
+    viewModel {FragmentCityViewModel(get()) }
 }
