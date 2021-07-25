8887ecdb85663b634084ba59eecb465d367809c2
Nurholis <holis.nur072219@gmail.com>
2021-07-25 20:12:28 +0700

    Adding FragmentViewState and viewModel

---
+++ app/src/main/java/com/example/cekongkir/presentation/ui/fragment/fragmenttrackingresult/FragmentTrackingResultViewModel.kt
@@ -0,0 +1,15 @@
+/**
+ * Cek Ongkir Android Apps
+ * Copyright (c) 2021. All rights reserved.
+ * Created by Nurholis on 25/7/21 20:09 PM
+ * Last modified 25/7/21 20:09 PM by Nurholis
+ * Licensed under the Apache Licence, Version 2.0
+ * May not copy files in their entirety for either distribution or
+ * rewriting of source code.*/
+package com.example.cekongkir.presentation.ui.fragment.fragmenttrackingresult
+
+import androidx.lifecycle.ViewModel
+
+class FragmentTrackingResultViewModel: ViewModel() {
+
+}
